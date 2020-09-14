7a4ffab1fbac97be35c159a8d02342afa67a7f7b
ty-vin <tyvin888@gmail.com>
2020-09-14 07:27:39 -0400

    updates to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,35 @@
-import { WelcomeCComponent } from './welcome/welcome-c/welcome-c.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CrownCComponent } from "./crown/crown-c/crown-c.component";
 
+import { CrownCComponent } from "./crown/crown-c/crown-c.component";
+import { HeartCComponent } from './heart/heart-c/heart-c.component';
+import { InfoCComponent } from './info/info-c/info-c.component';
+import { MainCComponent } from './main/main-c/main-c.component';
+import { RootCComponent } from './root/root-c/root-c.component';
+import { SacralCComponent } from './sacral/sacral-c/sacral-c.component';
+import { SolarCComponent } from './solar/solar-c/solar-c.component';
+import { ThirdeyeCComponent } from './thirdeye/thirdeye-c/thirdeye-c.component';
+import { ThroatCComponent } from './throat/throat-c/throat-c.component';
+import { WelcomeCComponent } from './welcome/welcome-c/welcome-c.component';
 
 
 
 const routes: Routes = [
 
- 
-
-  
+  { path: 'welcome', component: WelcomeCComponent },  
   { path: 'crown', component: CrownCComponent },
+  { path: 'heart', component: HeartCComponent },
+  { path: 'info', component: InfoCComponent },
+  { path: 'main', component: MainCComponent },
+  { path: 'root', component: RootCComponent },
+  { path: 'sacral', component: SacralCComponent },
+  { path: 'solar', component: SolarCComponent },
+  { path: 'thirdeye', component: ThirdeyeCComponent },
+  { path: 'throat', component: ThroatCComponent },
+  {path: '', redirectTo: '', pathMatch: 'full' }
+
+
+
   /* 
   { path: 'heart',  loadChildren: () => import('./heart/heart.module').then(m => m.HeartModule) },
   { path: 'info',  loadChildren: () => import('./info/info.module').then(m => m.InfoModule) },
@@ -21,7 +39,6 @@ const routes: Routes = [
   { path: 'solar',  loadChildren: () => import('./solar/solar.module').then(m => m.SolarModule) },
   { path: 'sacral',  loadChildren: () => import('./sacral/sacral.module').then(m => m.SacralModule) },
 */
-  { path: '', component: WelcomeCComponent }  
 
 
 ];
